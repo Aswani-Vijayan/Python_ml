80927e94577012ca84a29dabbcec67f457bc984c
Aswani-Vijayan <aswanivijayan18@gmail.com>
2023-04-24 17:12:15 +0530

    added quadratic file

---
+++ Quadratic.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value of a "))
+b = int(input("Enter the value of b "))
+c = int(input("Enter the value of c "))
+k = (b**2)-(4*a*c)
+d = k**0.5
+x1 = (-b+d)/(2*a)
+x2 = (-b-d)/(2*a)
+print(x1)
+print(x2)
